388390aec6e7d13147bac8e4754852ab85f8df48
NoahSantos <nsanto591@west-mec.org>
2022-11-08 16:58:54 -0700

    Background
    Got the background of the button to change when clicked, so it indicates which square belongs to which player.

--- index.js
+++ index.js
@@ -8,22 +8,21 @@ let board=[
 let turn=0;
 let xWin=0;
 let yWin=0;
-let space=0;
 
-//adds player piece the array
-function showBoard(x, y){
-    console.log(board);
-    if(checkSpace(x,y)){
-        if(turn%2==1){
-            board[x][y]="o";
-            return true;
-        }else{
-            board[x][y]="x";
-            return true;
-        }
-    }
-    return false;
-}
+// //adds player piece the array
+// function showBoard(x, y){
+//     console.log(board);
+//     if(checkSpace(x,y)){
+//         if(turn%2==1){
+//             board[x][y]="o";
+//             return true;
+//         }else{
+//             board[x][y]="x";
+//             return true;
+//         }
+//     }
+//     return false;
+// }
 
 //checks if the space is open
 function checkSpace(x, y){
@@ -35,36 +34,55 @@ function checkSpace(x, y){
 }
 
 //sets the turn and player
-function change(x,y){
-    if(showBoard(x,y)){
+function change(x,y, square){
+    if(checkSpace(x,y)){
+        if(turn%2==1){
+            // document.getElementById("player").innerHTML="O";
+            document.getElementById(square).innerHTML="O";
+            board[x][y]="o";
+        }else{
+            // document.getElementById("player").innerHTML="X";
+            document.getElementById(square).innerHTML="X";
+            board[x][y]="x";
+        }
         turn++;
         document.getElementById("turn").innerHTML=turn;
         if(turn%2==1){
             document.getElementById("player").innerHTML="O";
+            // board[x][y]="o";
         }else{
             document.getElementById("player").innerHTML="X";
+            // document.getElementById(square).innerHTML="X";
+            // board[x][y]="x";
         }
     }
+    // showBoard(x, y);
 }
 
+
+const CHECK = [0, 1, 2];
 //function to check for winner
 function checkWin(){
     // checks for verticle wins
-    if(board[space][space]==board[space+1][space] && board[space][space]==board[space+2][space] && board[space][space]=="X"){
-        xWin++;
-        domcument.getElementById("xWin").innerHTML=xWin;
-        console.log("X wins");
-    }else if(board[space][space]==board[space+1][space] && board[space][space]==board[space+2][space] && board[space][space]=="Y"){
-        yWin++;
-        domcument.getElementById("yWin").innerHTML=yWin; 
+    for(let i=0;i<3;i++){
+        if(board[0][i]==board[1][i] && board[0][i]==board[2][i] && board[0][i]=="X"){
+            xWin++;
+            document.getElementById("xWin").innerHTML=xWin;
+            console.log("X wins");
+        }else if(board[0][i]==board[1][i] && board[0][i]==board[2][i] && board[0][i]=="Y"){
+            yWin++;
+            document.getElementById("yWin").innerHTML=yWin; 
+        }
     }
     //checks for horizontal wins
-    if(board[space][space]==board[space][space+1] && board[space][space]==board[space][space+2] && board[space][space]=="X"){
-        xWin++;
-        domcument.getElementById("xWin").innerHTML=xWin;
-    }else if(board[space][space]==board[space][space+1] && board[space][space]==board[space][space+2] && board[space][space]=="Y"){
-        yWin++;
-        domcument.getElementById("yWin").innerHTML=yWin; 
+    for(let i=0;i<3;i++){
+        if(board[i][0]==board[i][1] && board[i][0]==board[i][2] && board[i][0]=="X"){
+            xWin++;
+            document.getElementById("xWin").innerHTML=xWin;
+        }else if(board[i][0]==board[i][1] && board[i][0]==board[i][2] && board[i][0]=="Y"){
+            yWin++;
+            document.getElementById("yWin").innerHTML=yWin; 
+        }
     }
 }
 
@@ -73,55 +91,54 @@ function checkWin(){
 function mark1(){
     let x =0;
     let y=0;
-    let square=1;
-    // (turn%2==0)?document.getElementById("square1").innerHTML="X":document.getElementById("square1").innerHTML="O";
-    change(x,y);
+    let square="sqr1";
+    change(x,y,square);
 }
 function mark2(){
     let x =0;
     let y=1;
-    let square=2;
-    change(x,y);
+    let square="sqr2";
+    change(x,y,square);
 }
 function mark3(){
     let x =0;
     let y=2;
-    let square=3;
-    change(x,y);
+    let square="sqr3";
+    change(x,y,square);
 }
 function mark4(){
     let x =1;
     let y=0;
-    let square=4;
-    change(x,y);
+    let square="sqr4";
+    change(x,y,square);
 }
 function mark5(){
     let x =1;
     let y=1;
-    let square=5;
-    change(x,y);
+    let square="sqr5";
+    change(x,y,square);
 }
 function mark6(){
     let x =1;
     let y=2;
-    let square=6;
-    change(x,y);
+    let square="sqr6";
+    change(x,y,square);
 }
 function mark7(){
     let x =2;
     let y=0;
-    let square=7;
-    change(x,y);
+    let square="sqr7";
+    change(x,y,square);
 }
 function mark8(){
     let x =2;
     let y=1;
-    let square=8;
-    change(x,y);
+    let square="sqr8";
+    change(x,y,square);
 }
 function mark9(){
     let x =2;
     let y=2;
-    let square=9;
-    change(x,y);
+    let square="sqr9";
+    change(x,y,square);
 }
